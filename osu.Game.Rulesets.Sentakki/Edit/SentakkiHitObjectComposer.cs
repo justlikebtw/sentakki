5aac8de3e26a1e16ace1ef02c69c0eedd07ada42
Derrick Timmermans <derrick.timmermans@outlook.com>
2020-06-27 13:52:36 +0800

    Implement selection handler for Touch notes

--- osu.Game.Rulesets.Sentakki/Edit/SentakkiHitObjectComposer.cs
+++ osu.Game.Rulesets.Sentakki/Edit/SentakkiHitObjectComposer.cs
@@ -2,10 +2,15 @@ using System.Collections.Generic;
 using osu.Game.Rulesets.Edit;
 using osu.Game.Rulesets.Edit.Tools;
 using osu.Game.Rulesets.Objects.Drawables;
+using osu.Framework.Input;
+using osu.Framework.Input.Events;
+using osuTK;
+using osuTK.Input;
 using osu.Game.Rulesets.Sentakki.Objects;
 using osu.Game.Screens.Edit.Compose.Components;
 using System;
 using osu.Framework.Graphics;
+using System.IO;
 
 namespace osu.Game.Rulesets.Sentakki.Edit
 {
@@ -27,5 +32,32 @@ namespace osu.Game.Rulesets.Sentakki.Edit
 
         protected override ComposeBlueprintContainer CreateBlueprintContainer()
             => new SentakkiBlueprintContainer(this);
+
+        protected override bool OnKeyDown(KeyDownEvent e)
+        {
+            if (e.Key >= Key.S)
+            {
+                FileInfo file = new FileInfo(@"C:\Users\pertr\Documents\ma2edit\sentakki\output.txt");
+                file.Directory.Create();
+                file.Delete();
+                using (StreamWriter sw = new StreamWriter(@"C:\Users\pertr\Documents\ma2edit\sentakki\output.txt"))
+                {
+                    foreach (SentakkiHitObject hitObject in EditorBeatmap.HitObjects)
+                    {
+                        switch (hitObject)
+                        {
+                            case Tap tap:
+                                sw.WriteLine("TAP " + tap.Angle.GetNotePathFromDegrees() + " " + tap.StartTime.ToString());
+                                break;
+                            case Hold hold:
+                                sw.WriteLine("HLD " + hold.Angle.GetNotePathFromDegrees() + " " + hold.StartTime.ToString() + " " + hold.Duration.ToString());
+                                break;
+                        }
+                    }
+                }
+                return true;
+            }
+            return base.OnKeyDown(e);
+        }
     }
 }
